2ecf40304f459f1a69e6ce597e8ba5cf5bc9c684
Ahnaf Hasan <ahasan1@stuy.edu>
2019-01-12 22:54:27 -0500

    worked with adil

--- sys/forking_server.c
+++ sys/forking_server.c
@@ -12,7 +12,7 @@ void subserver(int from_client, char * person);
     }
   }}
 int main() {
-
+  fill();
   int listen_socket;
   int f;
   listen_socket = server_setup();
@@ -45,16 +45,21 @@ void subserver(int client_socket, char * who) {
 }
 
 void process(char * s, char * per) {
-  if (atoi(s) > COLS || atoi(s) < 1) {
+  if (atoi(s) > COLS || atoi(s) - 1 < 0) {
       char * tmp = strcpy(s, "Invalid number, try again");
       print(board);
       return;
     }
   if (!strcmp(per, "X")) {
-    int num = atoi(s);
+    int num = atoi(s) - 1;
     char * tmp = board[num];
-    int index = strchr(tmp, '_');
-    board[num][index] = 'X';
+    for(int r = 5; r >= 0; r--){
+      if(board[r][num] == '_'){
+        board[r][num] = 'X';
+        break;
+      }
+    }
+    print(board);
     int finished = connected(board);
     if (finished) {
       strcpy(s, "Player X has won the game!\n");
@@ -64,8 +69,12 @@ void process(char * s, char * per) {
   }else{
     int num = atoi(s);
     char * tmp = board[num];
-    int index = strchr(tmp, '_');
-    board[num][index] = 'O';
+    for(int r = 5; r >= 0; r--){
+    if(board[r][num] == '_'){
+      board[r][num] = 'O';
+      break;
+    }
+  }
     int finished = connected(board);
     if (finished) {
       strcpy(s, "Player O has won the game!\n");
